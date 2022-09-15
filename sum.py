e6b04471cc117b88480003af9b696598c6a665a4
tweathers1 <112443029+tweathers1@users.noreply.github.com>
2022-09-14 22:40:04 -0400

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,7 @@
+echo "# HW1" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add TWs_HW1 https://github.com/tweathers1/HW1.git
+git push -u origin main
